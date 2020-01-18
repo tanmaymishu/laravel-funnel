0e6a31211af59e1098378052e05f4b3cc485aa2f
Tanmay Mishu <tanmaymishu@gmail.com>
2020-01-18 21:16:43 +0600

    Add laravel support and service provider

---
+++ src/FunnelServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TanmayMishu\LaravelFunnel;
+
+use Illuminate\Support\ServiceProvider;
+
+class FunnelServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
