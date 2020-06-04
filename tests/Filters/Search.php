59df1aadbba46d8426880197504ee49707d44cf0
Tanmay Mishu <tanmaymishu@gmail.com>
2020-06-04 15:42:36 +0600

    Simplify where stub

--- tests/Filters/Search.php
+++ tests/Filters/Search.php
@@ -30,46 +30,6 @@ class Search extends Filter
      */
     protected function apply(Builder $builder): Builder
     {
-        $paramValue = $this->getParamValue();
-
-        if (is_array($paramValue)) {
-            $builder = $builder->where(function ($q) use ($paramValue) {
-                for ($i = 0; $i < count($paramValue); $i++) {
-                    if ($i == 0) {
-                        $q->where($this->attribute, $this->operator, $paramValue[$i]);
-                    } else {
-                        $q->orWhere($this->attribute, $this->operator, $paramValue[$i]);
-                    }
-                }
-            });
-
-            return $builder;
-        }
-
-        return $builder->where($this->attribute, $this->operator, $paramValue);
-    }
-
-    /**
-     * Get the parameter's value. If the parameter is an array
-     * and the operator is like/LIKE, then surround the values
-     * with `%` and return the modified array.
-     *
-     * @return string|array
-     */
-    private function getParamValue()
-    {
-        $params = request($this->parameter);
-
-        if (is_array($params) && ($this->operator == 'LIKE' || $this->operator == 'like')) {
-            $params = array_map(function ($param) {
-                return '%'.$param.'%';
-            }, $params);
-
-            return $params;
-        }
-
-        return $this->operator == 'LIKE' || $this->operator == 'like'
-            ? '%'.$params.'%'
-            : $params;
+        return $this->getDefaultWhereBuilder($builder);
     }
 }
