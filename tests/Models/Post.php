c46848a1cf1cfa9cb7eb43a2dbab3e4f902b1e71
Tanmay Mishu <tanmaymishu@gmail.com>
2020-06-07 11:02:00 +0600

    Apply fixes from StyleCI (#6)

--- tests/Models/Post.php
+++ tests/Models/Post.php
@@ -4,11 +4,11 @@ namespace TanmayMishu\Tests\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use TanmayMishu\LaravelFunnel\HasFilters;
+use TanmayMishu\Tests\Filters\Comment;
 use TanmayMishu\Tests\Filters\Published;
+use TanmayMishu\Tests\Filters\Reply;
 use TanmayMishu\Tests\Filters\TitleMatch;
 use TanmayMishu\Tests\Filters\TitleSearch;
-use TanmayMishu\Tests\Filters\Comment;
-use TanmayMishu\Tests\Filters\Reply;
 
 class Post extends Model
 {
